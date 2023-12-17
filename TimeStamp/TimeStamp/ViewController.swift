c971ece0ae1c965b50367a1c1f4e49fb045fcf50
Tawan-Github <raphat02.mainirl@gmail.com>
2023-12-17 18:08:33 +0700

    upload project files

---
+++ TimeStamp/TimeStamp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeStamp
+//
+//  Created by Raphat Aektantidamrong on 17/12/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
